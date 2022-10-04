5be5637713e592c2cbff9a522f9b62520b64b6a8
Alejandro Salas <asalas.wd@outlook.com>
2022-10-03 21:14:23 -0400

    Complete MarsApplication to create MarsRobot object and update attributes

--- src/MarsApplication.java
+++ src/MarsApplication.java
@@ -1,2 +1,19 @@
-public class MarsApplication {
+class MarsApplication {
+    public static void main(String[] args) {
+        MarsRobot spirit = new MarsRobot();
+        spirit.status = "exploring";
+        spirit.speed = 2;
+        spirit.temperature = -60;
+
+        spirit.showAttributes();
+        System.out.println("Increasing speed to 3.");
+        spirit.speed = 3;
+        spirit.showAttributes();
+        System.out.println("Changing temperature to -90.");
+        spirit.temperature = -90;
+        spirit.showAttributes();
+        System.out.println("Checking the temperature.");
+        spirit.checkTemperature();
+        spirit.showAttributes();
+    }
 }
