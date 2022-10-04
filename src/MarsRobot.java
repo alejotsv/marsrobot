412cb6f689e8fbb2175c390fbb9ece9651f06042
Alejandro Salas <asalas.wd@outlook.com>
2022-10-03 20:40:01 -0400

    Add attributes and methods

--- src/MarsRobot.java
+++ src/MarsRobot.java
@@ -1,2 +1,18 @@
 class MarsRobot {
+    String status;
+    int speed;
+    float temperature;
+
+    void checkTemperature() {
+        if (temperature < 80) {
+            status = "returning home";
+            speed = 5;
+        }
+    }
+
+    void showAttributes() {
+        System.out.println("Status: " + status);
+        System.out.println("Speed: " + speed);
+        System.out.println("Temperature: " + temperature);
+    }
 }
